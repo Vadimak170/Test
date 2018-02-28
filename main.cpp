7522518f35a5e1b961c3a9c889f9b0a3fc6347b2
Vadimak170 <vadimak170@gmail.com>
2018-03-01 02:20:15 +0600

    Так, когда я уже разберусь

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <windef.h>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
-    cout<<"MDAMDAMDA"<<endl;
     cout<<"sdasa"<endl;
     return 0;
 }
